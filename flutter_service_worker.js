fe32ae0b7653a56380464b79cd007a3d02206612
Techi Samudio <mtsamudiog@gmail.com>
2022-07-25 20:55:32 -0400

    add Lottie animation

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "412f896e9c079a1e82839a54439cea69",
-"index.html": "92d18f589403dc979fc9aa0c2f840840",
-"/": "92d18f589403dc979fc9aa0c2f840840",
-"main.dart.js": "a9b32a601f89a64d4667e3bf17a5a9af",
+"index.html": "e9f55bfb8a67de4f246587770c3800f9",
+"/": "e9f55bfb8a67de4f246587770c3800f9",
+"main.dart.js": "493d4e25b5d3c2184d21f06c8bc81d45",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
